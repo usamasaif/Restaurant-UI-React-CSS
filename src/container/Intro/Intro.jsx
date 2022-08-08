3e6b872ae4c4461ce4e3bedf8fae19571625955f
usamasaif <usamasaif350@gmail.com>
2022-08-08 18:48:04 +0500

    Awards section added

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 
-import './Intro.css';
+import { meal } from "../../constants";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import "./Intro.css";
+
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prev) => !prev);
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+
+  return (
+    <section className="app__video">
+      <video
+        className="video"
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Intro;
